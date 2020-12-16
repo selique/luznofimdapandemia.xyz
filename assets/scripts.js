62d60615f79763d00014ddc82f35db3cf7ebeb9f
Moises Lopes Ferreira <moises.ferreira@altran.com.br>
2020-12-16 14:44:37 -0300

    try fix tweak
    t

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,16 @@
-$('.grid').masonry({
-    // options
-    columnWidth: '.grid-item',
-    itemSelector: '.grid-item',
-    percentPosition: true
+$(document).ready(function(){
+    $('img').load(function(){
+        $('.grid').masonry({
+            // options
+            columnWidth: '.grid-item',
+            itemSelector: '.grid-item',
+            percentPosition: true
+        });
+    });
+    $('.grid').masonry({
+        // options
+        columnWidth: '.grid-item',
+        itemSelector: '.grid-item',
+        percentPosition: true
+    });
 });
