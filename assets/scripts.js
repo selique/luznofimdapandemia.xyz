f3007eb4131fc6fd82acc6c16662007d197e2511
Moises Lopes Ferreira <moises.ferreira@altran.com.br>
2020-12-16 14:48:28 -0300

    Try fix to footer

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,2 @@
-$(document).ready(function(){
-    $('img').load(function(){
-        $('.grid').masonry({
-            // options
-            columnWidth: '.grid-item',
-            itemSelector: '.grid-item',
-            percentPosition: true
-        });
-    });
-    $('.grid').masonry({
-        // options
-        columnWidth: '.grid-item',
-        itemSelector: '.grid-item',
-        percentPosition: true
-    });
-});
+
+
